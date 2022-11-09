b8325d8236bf6c398a4901ea67e33935a3365151
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2022-11-09 14:03:14 -0300

    Criacao estrutura de pastas + arquivos RecQS e medQS

---
+++ include/medQuicksort.hpp
@@ -0,0 +1,13 @@
+#ifndef QUICKSORT_MED
+#define QUICKSORT_MED
+
+#include "dados.hpp"
+
+void Particao(int Esq, int Dir, int *i, int *j, Item *A);
+void Ordena(int Esq, int Dir, Item *A);
+void medQuickSort(Item *A, int n);
+
+
+
+
+#endif
